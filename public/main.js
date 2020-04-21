320c0071fe8b0e1e5dec5b98c355e8d5d787b860
Rahul Pandya <rahul25796@gmail.com>
2020-04-21 02:23:45 -0400

    updated main.js

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // const io = require("socket.io-client");
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://my-chat-place.herokuapp.com");
 
 socket.on("message", (data) => {
   console.log(`got your data: ${JSON.stringify(data)}`);

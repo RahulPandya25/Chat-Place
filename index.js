d682e896e142017ab1c1376f57598d8bb8e3d49d
Rahul Pandya <rahul25796@gmail.com>
2020-04-21 01:43:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+server.listen(PORT, () => {
+  console.log(`Server Running on PORT: ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected`);
+  socket.emit("message", { server: "how are you?" });
+  socket.on("reply-msg", (data) => {
+    console.log(`got your data: ${JSON.stringify(data)}`);
+  });
+});
---
+++ public/main.js
@@ -0,0 +1,7 @@
+// const io = require("socket.io-client");
+const socket = io.connect("http://localhost:3000");
+
+socket.on("message", (data) => {
+  console.log(`got your data: ${JSON.stringify(data)}`);
+  socket.emit("reply-msg", { client: "I'm fine, how are you?" });
+});
